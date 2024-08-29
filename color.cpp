8619c5c36af7c06c7fc815847dca8ddb77c5a870
Sander Speetjens <sander.speetjens@gmail.com>
2024-08-29 14:55:07 +0200

    Lint

--- color.cpp
+++ color.cpp
@@ -1,25 +1,25 @@
 /*
  * MIT License
- * 
+ *
  * Copyright (c) 2024 Sander Speetjens
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE. 
+ * SOFTWARE.
  */
 #include "color.hpp"
 
@@ -47,302 +47,302 @@ Color::Color(Colors color) {
         case Colors::Aquamarine:
             set_color(0x70, 0xDB, 0x93);
             break;
-            case Colors::Baker_s_Chocolate:
-                set_color(0x5C, 0x33, 0x17);
+        case Colors::Baker_s_Chocolate:
+            set_color(0x5C, 0x33, 0x17);
             break;
-            case Colors::Black:
-                set_color(0x00, 0x00, 0x00);
+        case Colors::Black:
+            set_color(0x00, 0x00, 0x00);
             break;
-            case Colors::Blue:
-                set_color(0x00, 0x00, 0xFF);
+        case Colors::Blue:
+            set_color(0x00, 0x00, 0xFF);
             break;
-            case Colors::Blue_Violet:
-                set_color(0x9F, 0x5F, 0x9F);
+        case Colors::Blue_Violet:
+            set_color(0x9F, 0x5F, 0x9F);
             break;
-            case Colors::Brass:
-                set_color(0xB5, 0xA6, 0x42);
+        case Colors::Brass:
+            set_color(0xB5, 0xA6, 0x42);
             break;
-            case Colors::Bright_Gold:
-                set_color(0xD9, 0xD9, 0x19);
+        case Colors::Bright_Gold:
+            set_color(0xD9, 0xD9, 0x19);
             break;
-            case Colors::Brown:
-                set_color(0xA6, 0x2A, 0x2A);
+        case Colors::Brown:
+            set_color(0xA6, 0x2A, 0x2A);
             break;
-            case Colors::Bronze:
-                set_color(0x8C, 0x78, 0x53);
+        case Colors::Bronze:
+            set_color(0x8C, 0x78, 0x53);
             break;
-            case Colors::Bronze_II:
-                set_color(0xA6, 0x7D, 0x3D);
+        case Colors::Bronze_II:
+            set_color(0xA6, 0x7D, 0x3D);
             break;
-            case Colors::Cadet_Blue:
-                set_color(0x5F, 0x9F, 0x9F);
+        case Colors::Cadet_Blue:
+            set_color(0x5F, 0x9F, 0x9F);
             break;
-            case Colors::Cool_Copper:
-                set_color(0xD9, 0x87, 0x19);
+        case Colors::Cool_Copper:
+            set_color(0xD9, 0x87, 0x19);
             break;
-            case Colors::Copper:
-                set_color(0xB8, 0x73, 0x33);
+        case Colors::Copper:
+            set_color(0xB8, 0x73, 0x33);
             break;
-            case Colors::Coral:
-                set_color(0xFF, 0x7F, 0x00);
+        case Colors::Coral:
+            set_color(0xFF, 0x7F, 0x00);
             break;
-            case Colors::Corn_Flower_Blue:
-                set_color(0x42, 0x42, 0x6F);
+        case Colors::Corn_Flower_Blue:
+            set_color(0x42, 0x42, 0x6F);
             break;
-            case Colors::Cyan:
-                set_color(0x00, 0xFF, 0xFF);
+        case Colors::Cyan:
+            set_color(0x00, 0xFF, 0xFF);
             break;
-            case Colors::Dark_Brown:
-                set_color(0x5C, 0x40, 0x33);
+        case Colors::Dark_Brown:
+            set_color(0x5C, 0x40, 0x33);
             break;
-            case Colors::Dark_Green:
-                set_color(0x2F, 0x4F, 0x2F);
+        case Colors::Dark_Green:
+            set_color(0x2F, 0x4F, 0x2F);
             break;
-            case Colors::Dark_Green_Copper:
-                set_color(0x4A, 0x76, 0x6E);
+        case Colors::Dark_Green_Copper:
+            set_color(0x4A, 0x76, 0x6E);
             break;
-            case Colors::Dark_Olive_Green:
-                set_color(0x4F, 0x4F, 0x2F);
+        case Colors::Dark_Olive_Green:
+            set_color(0x4F, 0x4F, 0x2F);
             break;
-            case Colors::Dark_Orchid:
-                set_color(0x99, 0x32, 0xCD);
+        case Colors::Dark_Orchid:
+            set_color(0x99, 0x32, 0xCD);
             break;
-            case Colors::Dark_Purple:
-                set_color(0x87, 0x1F, 0x78);
+        case Colors::Dark_Purple:
+            set_color(0x87, 0x1F, 0x78);
             break;
-            case Colors::Dark_Slate_Blue:
-                set_color(0x6B, 0x23, 0x8E);
+        case Colors::Dark_Slate_Blue:
+            set_color(0x6B, 0x23, 0x8E);
             break;
-            case Colors::Dark_Slate_Grey:
-                set_color(0x2F, 0x4F, 0x4F);
+        case Colors::Dark_Slate_Grey:
+            set_color(0x2F, 0x4F, 0x4F);
             break;
-            case Colors::Dark_Tan:
-                set_color(0x97, 0x69, 0x4F);
+        case Colors::Dark_Tan:
+            set_color(0x97, 0x69, 0x4F);
             break;
-            case Colors::Dark_Turquoise:
-                set_color(0x70, 0x93, 0xDB);
+        case Colors::Dark_Turquoise:
+            set_color(0x70, 0x93, 0xDB);
             break;
-            case Colors::Dark_Wood:
-                set_color(0x85, 0x5E, 0x42);
+        case Colors::Dark_Wood:
+            set_color(0x85, 0x5E, 0x42);
             break;
-            case Colors::Dim_Grey:
-                set_color(0x54, 0x54, 0x54);
+        case Colors::Dim_Grey:
+            set_color(0x54, 0x54, 0x54);
             break;
-            case Colors::Dusty_Rose:
-                set_color(0x85, 0x63, 0x63);
+        case Colors::Dusty_Rose:
+            set_color(0x85, 0x63, 0x63);
             break;
-            case Colors::Feldspar:
-                set_color(0xD1, 0x92, 0x75);
+        case Colors::Feldspar:
+            set_color(0xD1, 0x92, 0x75);
             break;
-            case Colors::Firebrick:
-                set_color(0x8E, 0x23, 0x23);
+        case Colors::Firebrick:
+            set_color(0x8E, 0x23, 0x23);
             break;
-            case Colors::Forest_Green:
-                set_color(0x23, 0x8E, 0x23);
+        case Colors::Forest_Green:
+            set_color(0x23, 0x8E, 0x23);
             break;
-            case Colors::Gold:
-                set_color(0xCD, 0x7F, 0x32);
+        case Colors::Gold:
+            set_color(0xCD, 0x7F, 0x32);
             break;
-            case Colors::Goldenrod:
-                set_color(0xDB, 0xDB, 0x70);
+        case Colors::Goldenrod:
+            set_color(0xDB, 0xDB, 0x70);
             break;
-            case Colors::Grey:
-                set_color(0xC0, 0xC0, 0xC0);
+        case Colors::Grey:
+            set_color(0xC0, 0xC0, 0xC0);
             break;
-            case Colors::Green:
-                set_color(0x00, 0xFF, 0x00);
+        case Colors::Green:
+            set_color(0x00, 0xFF, 0x00);
             break;
-            case Colors::Green_Copper:
-                set_color(0x52, 0x7F, 0x76);
+        case Colors::Green_Copper:
+            set_color(0x52, 0x7F, 0x76);
             break;
-            case Colors::Green_Yellow:
-                set_color(0x93, 0xDB, 0x70);
+        case Colors::Green_Yellow:
+            set_color(0x93, 0xDB, 0x70);
             break;
-            case Colors::Hunter_Green:
-                set_color(0x21, 0x5E, 0x21);
+        case Colors::Hunter_Green:
+            set_color(0x21, 0x5E, 0x21);
             break;
-            case Colors::Indian_Red:
-                set_color(0x4E, 0x2F, 0x2F);
+        case Colors::Indian_Red:
+            set_color(0x4E, 0x2F, 0x2F);
             break;
-            case Colors::Khaki:
-                set_color(0x9F, 0x9F, 0x5F);
+        case Colors::Khaki:
+            set_color(0x9F, 0x9F, 0x5F);
             break;
-            case Colors::Light_Blue:
-                set_color(0xC0, 0xD9, 0xD9);
+        case Colors::Light_Blue:
+            set_color(0xC0, 0xD9, 0xD9);
             break;
-            case Colors::Light_Grey:
-                set_color(0xA8, 0xA8, 0xA8);
+        case Colors::Light_Grey:
+            set_color(0xA8, 0xA8, 0xA8);
             break;
-            case Colors::Light_Steel_Blue:
-                set_color(0x8F, 0x8F, 0xBD);
+        case Colors::Light_Steel_Blue:
+            set_color(0x8F, 0x8F, 0xBD);
             break;
-            case Colors::Light_Wood:
-                set_color(0xE9, 0xC2, 0xA6);
+        case Colors::Light_Wood:
+            set_color(0xE9, 0xC2, 0xA6);
             break;
-            case Colors::Lime_Green:
-                set_color(0x32, 0xCD, 0x32);
+        case Colors::Lime_Green:
+            set_color(0x32, 0xCD, 0x32);
             break;
-            case Colors::Magenta:
-                set_color(0xFF, 0x00, 0xFF);
+        case Colors::Magenta:
+            set_color(0xFF, 0x00, 0xFF);
             break;
-            case Colors::Mandarian_Orange:
-                set_color(0xE4, 0x78, 0x33);
+        case Colors::Mandarian_Orange:
+            set_color(0xE4, 0x78, 0x33);
             break;
-            case Colors::Maroon:
-                set_color(0x8E, 0x23, 0x6B);
+        case Colors::Maroon:
+            set_color(0x8E, 0x23, 0x6B);
             break;
-            case Colors::Medium_Aquamarine:
-                set_color(0x32, 0xCD, 0x99);
+        case Colors::Medium_Aquamarine:
+            set_color(0x32, 0xCD, 0x99);
             break;
-            case Colors::Medium_Blue:
-                set_color(0x32, 0x32, 0xCD);
+        case Colors::Medium_Blue:
+            set_color(0x32, 0x32, 0xCD);
             break;
-            case Colors::Medium_Forest_Green:
-                set_color(0x6B, 0x8E, 0x23);
+        case Colors::Medium_Forest_Green:
+            set_color(0x6B, 0x8E, 0x23);
             break;
-            case Colors::Medium_Goldenrod:
-                set_color(0xEA, 0xEA, 0xAE);
+        case Colors::Medium_Goldenrod:
+            set_color(0xEA, 0xEA, 0xAE);
             break;
-            case Colors::Medium_Orchid:
-                set_color(0x93, 0x70, 0xDB);
+        case Colors::Medium_Orchid:
+            set_color(0x93, 0x70, 0xDB);
             break;
-            case Colors::Medium_Seagreen:
-                set_color(0x42, 0x6F, 0x42);
+        case Colors::Medium_Seagreen:
+            set_color(0x42, 0x6F, 0x42);
             break;
-            case Colors::Medium_Slate_Blue:
-                set_color(0x7F, 0x00, 0xFF);
+        case Colors::Medium_Slate_Blue:
+            set_color(0x7F, 0x00, 0xFF);
             break;
-            case Colors::Medium_Spring_Green:
-                set_color(0x7F, 0xFF, 0x00);
+        case Colors::Medium_Spring_Green:
+            set_color(0x7F, 0xFF, 0x00);
             break;
-            case Colors::Medium_Turquoise:
-                set_color(0x70, 0xDB, 0xDB);
+        case Colors::Medium_Turquoise:
+            set_color(0x70, 0xDB, 0xDB);
             break;
-            case Colors::Medium_Violet_Red:
-                set_color(0xDB, 0x70, 0x93);
+        case Colors::Medium_Violet_Red:
+            set_color(0xDB, 0x70, 0x93);
             break;
-            case Colors::Medium_Wood:
-                set_color(0xA6, 0x80, 0x64);
+        case Colors::Medium_Wood:
+            set_color(0xA6, 0x80, 0x64);
             break;
-            case Colors::Midnight_Blue:
-                set_color(0x2F, 0x2F, 0x4F);
+        case Colors::Midnight_Blue:
+            set_color(0x2F, 0x2F, 0x4F);
             break;
-            case Colors::Navy_Blue:
-                set_color(0x23, 0x23, 0x8E);
+        case Colors::Navy_Blue:
+            set_color(0x23, 0x23, 0x8E);
             break;
-            case Colors::Neon_Blue:
-                set_color(0x4D, 0x4D, 0xFF);
+        case Colors::Neon_Blue:
+            set_color(0x4D, 0x4D, 0xFF);
             break;
-            case Colors::Neon_Pink:
-                set_color(0xFF, 0x6E, 0xC7);
+        case Colors::Neon_Pink:
+            set_color(0xFF, 0x6E, 0xC7);
             break;
-            case Colors::New_Midnight_Blue:
-                set_color(0x00, 0x00, 0x9C);
+        case Colors::New_Midnight_Blue:
+            set_color(0x00, 0x00, 0x9C);
             break;
-            case Colors::New_Tan:
-                set_color(0xEB, 0xC7, 0x9E);
+        case Colors::New_Tan:
+            set_color(0xEB, 0xC7, 0x9E);
             break;
-            case Colors::Old_Gold:
-                set_color(0xCF, 0xB5, 0x3B);
+        case Colors::Old_Gold:
+            set_color(0xCF, 0xB5, 0x3B);
             break;
-            case Colors::Orange:
-                set_color(0xFF, 0x7F, 0x00);
+        case Colors::Orange:
+            set_color(0xFF, 0x7F, 0x00);
             break;
-            case Colors::Orange_Red:
-                set_color(0xFF, 0x24, 0x00);
+        case Colors::Orange_Red:
+            set_color(0xFF, 0x24, 0x00);
             break;
-            case Colors::Orchid:
-                set_color(0xDB, 0x70, 0xDB);
+        case Colors::Orchid:
+            set_color(0xDB, 0x70, 0xDB);
             break;
-            case Colors::Pale_Green:
-                set_color(0x8F, 0xBC, 0x8F);
+        case Colors::Pale_Green:
+            set_color(0x8F, 0xBC, 0x8F);
             break;
-            case Colors::Pink:
-                set_color(0xBC, 0x8F, 0x8F);
+        case Colors::Pink:
+            set_color(0xBC, 0x8F, 0x8F);
             break;
-            case Colors::Plum:
-                set_color(0xEA, 0xAD, 0xEA);
+        case Colors::Plum:
+            set_color(0xEA, 0xAD, 0xEA);
             break;
-            case Colors::Quartz:
-                set_color(0xD9, 0xD9, 0xF3);
+        case Colors::Quartz:
+            set_color(0xD9, 0xD9, 0xF3);
             break;
-            case Colors::Red:
-                set_color(0xFF, 0x00, 0x00);
+        case Colors::Red:
+            set_color(0xFF, 0x00, 0x00);
             break;
-            case Colors::Rich_Blue:
-                set_color(0x59, 0x59, 0xAB);
+        case Colors::Rich_Blue:
+            set_color(0x59, 0x59, 0xAB);
             break;
-            case Colors::Salmon:
-                set_color(0x6F, 0x42, 0x42);
+        case Colors::Salmon:
+            set_color(0x6F, 0x42, 0x42);
             break;
-            case Colors::Scarlet:
-                set_color(0x8C, 0x17, 0x17);
+        case Colors::Scarlet:
+            set_color(0x8C, 0x17, 0x17);
             break;
-            case Colors::Sea_Green:
-                set_color(0x23, 0x8E, 0x68);
+        case Colors::Sea_Green:
+            set_color(0x23, 0x8E, 0x68);
             break;
-            case Colors::Semi_Sweet_Chocolate:
-                set_color(0x6B, 0x42, 0x26);
+        case Colors::Semi_Sweet_Chocolate:
+            set_color(0x6B, 0x42, 0x26);
             break;
-            case Colors::Sienna:
-                set_color(0x8E, 0x6B, 0x23);
+        case Colors::Sienna:
+            set_color(0x8E, 0x6B, 0x23);
             break;
-            case Colors::Silver:
-                set_color(0xE6, 0xE8, 0xFA);
+        case Colors::Silver:
+            set_color(0xE6, 0xE8, 0xFA);
             break;
-            case Colors::Sky_Blue:
-                set_color(0x32, 0x99, 0xCC);
+        case Colors::Sky_Blue:
+            set_color(0x32, 0x99, 0xCC);
             break;
-            case Colors::Slate_Blue:
-                set_color(0x00, 0x7F, 0xFF);
+        case Colors::Slate_Blue:
+            set_color(0x00, 0x7F, 0xFF);
             break;
-            case Colors::Spicy_Pink:
-                set_color(0xFF, 0x1C, 0xAE);
+        case Colors::Spicy_Pink:
+            set_color(0xFF, 0x1C, 0xAE);
             break;
-            case Colors::Spring_Green:
-                set_color(0x00, 0xFF, 0x7F);
+        case Colors::Spring_Green:
+            set_color(0x00, 0xFF, 0x7F);
             break;
-            case Colors::Steel_Blue:
-                set_color(0x23, 0x6B, 0x8E);
+        case Colors::Steel_Blue:
+            set_color(0x23, 0x6B, 0x8E);
             break;
-            case Colors::Summer_Sky:
-                set_color(0x38, 0xB0, 0xDE);
+        case Colors::Summer_Sky:
+            set_color(0x38, 0xB0, 0xDE);
             break;
-            case Colors::Tan:
-                set_color(0xDB, 0x93, 0x70);
+        case Colors::Tan:
+            set_color(0xDB, 0x93, 0x70);
             break;
-            case Colors::Thistle:
-                set_color(0xD8, 0xBF, 0xD8);
+        case Colors::Thistle:
+            set_color(0xD8, 0xBF, 0xD8);
             break;
-            case Colors::Turquoise:
-                set_color(0xAD, 0xEA, 0xEA);
+        case Colors::Turquoise:
+            set_color(0xAD, 0xEA, 0xEA);
             break;
-            case Colors::Very_Dark_Brown:
-                set_color(0x5C, 0x40, 0x33);
+        case Colors::Very_Dark_Brown:
+            set_color(0x5C, 0x40, 0x33);
             break;
-            case Colors::Very_Light_Grey:
-                set_color(0xCD, 0xCD, 0xCD);
+        case Colors::Very_Light_Grey:
+            set_color(0xCD, 0xCD, 0xCD);
             break;
-            case Colors::Violet:
-                set_color(0x4F, 0x2F, 0x4F);
+        case Colors::Violet:
+            set_color(0x4F, 0x2F, 0x4F);
             break;
-            case Colors::Violet_Red:
-                set_color(0xCC, 0x32, 0x99);
+        case Colors::Violet_Red:
+            set_color(0xCC, 0x32, 0x99);
             break;
-            case Colors::Wheat:
-                set_color(0xD8, 0xD8, 0xBF);
+        case Colors::Wheat:
+            set_color(0xD8, 0xD8, 0xBF);
             break;
-            case Colors::White:
-                set_color(0xFF, 0xFF, 0xFF);
+        case Colors::White:
+            set_color(0xFF, 0xFF, 0xFF);
             break;
-            case Colors::Yellow:
-                set_color(0xFF, 0xFF, 0x00);
+        case Colors::Yellow:
+            set_color(0xFF, 0xFF, 0x00);
             break;
-            case Colors::Yellow_Green:
-                set_color(0x99, 0xCC, 0x32);
+        case Colors::Yellow_Green:
+            set_color(0x99, 0xCC, 0x32);
             break;
-            case Colors::Dark_Yellow:
-                set_color(0x99, 0xCC, 0x32);
+        case Colors::Dark_Yellow:
+            set_color(0x99, 0xCC, 0x32);
             break;
     }
 }
@@ -376,7 +376,7 @@ void Color::set_red(uint8_t color) {
 }
 
 void Color::set_green(uint8_t color) {
-     m_color = (color << 8) | (m_color & 0xFFFF00FF);
+    m_color = (color << 8) | (m_color & 0xFFFF00FF);
 }
 
 void Color::set_blue(uint8_t color) {
@@ -406,10 +406,10 @@ std::string Color::to_hex(hex_type type) const {
     return std::string(buffer);
 }
 
-bool Color::operator==(const Color &color) const {
+bool Color::operator==(const Color& color) const {
     return m_color == color.m_color;
 }
 
-bool Color::operator!=(const Color &color) const {
+bool Color::operator!=(const Color& color) const {
     return m_color != color.m_color;
 }
--- color.hpp
+++ color.hpp
@@ -1,30 +1,30 @@
 /*
  * MIT License
- * 
+ *
  * Copyright (c) 2024 Sander Speetjens
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE. 
+ * SOFTWARE.
  */
 #pragma once
 
-#include <string>
 #include <cstdint>
+#include <string>
 
 class Color {
   public:
@@ -131,12 +131,7 @@ class Color {
         Dark_Yellow
     };
 
-    enum hex_type {
-        HEX_TYPE_RGB,
-        HEX_TYPE_ARGB,
-        HEX_TYPE_RGB_HASH,
-        HEX_TYPE_ARGB_HASH
-    };
+    enum hex_type { HEX_TYPE_RGB, HEX_TYPE_ARGB, HEX_TYPE_RGB_HASH, HEX_TYPE_ARGB_HASH };
     Color();
     Color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha = 0xFF);
     Color(uint32_t color);
@@ -157,8 +152,9 @@ class Color {
 
     std::string to_hex(hex_type type = HEX_TYPE_ARGB_HASH) const;
 
-    bool operator==(const Color &color) const;
-    bool operator!=(const Color &color) const;
+    bool operator==(const Color& color) const;
+    bool operator!=(const Color& color) const;
+
   private:
     uint32_t m_color;
 };
